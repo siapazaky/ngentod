e8fa277f6b17a69bf6177f9c9efea21450726134
Ahmed Rangel <ahmedrangel@outlook.com>
2023-10-03 12:53:23 -0500

    twitter scraper now supprot x.com urls

--- src/index.js
+++ src/index.js
@@ -1843,7 +1843,7 @@ router.get("/dc/twitter-video-scrapper?", async (req, env) => {
   const { query } = req;
   const url = decodeURIComponent(query.url);
   const graphql = "https://twitter.com/i/api/graphql/xOhkmRac04YFZmOzU9PJHg";
-  if (url.includes("twitter.com/")) {
+  if (url.includes("twitter.com/") || url.includes("x.com/")) {
     console.log("es link de twitter");
     const id = obtenerIDDesdeURL(url);
     const tweetQuery = `TweetDetail?variables={"focalTweetId":"${id}","with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true,"withV2Timeline":true}&features={"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}&fieldToggles={"withArticleRichContentState":false}`;
