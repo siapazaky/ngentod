95cfd7caedb92231a3aab8efc4953d2d34c4bda2
Ahmed Rangel <ahmedrangel@outlook.com>
2023-11-04 11:06:22 -0500

    Update nightbotEmotes.js

--- src/utils/nightbotEmotes.js
+++ src/utils/nightbotEmotes.js
@@ -1,3 +1,4 @@
 export const nbFuck = [":3","angarRico","SEGS","yoooo","PawgChamp","HYPERSALAMI"];
 export const nbHug = [":3","ayayaxdd","SNIFFA","peepoShy","danse","BegWan"];
-export const nbKiss = [":3","catKISS","LICKA","xqcL","SLORPGLORPIN","yoooo","ayayaxdd","angarlolero","peepoShy","BegWan"];
+export const nbKissChino = [":3","catKISS","LICKA","xqcL","SLORPGLORPIN","yoooo","ayayaxdd","angarlolero","peepoShy","BegWan"];
+export const nbKiss = [":3","catKISS","LICKA","ayayaxdd","angarlolero","peepoShy","BegWan"];
